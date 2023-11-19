1544a5e7a861180312a7f9e0f9e2dfd9b8d72c59
Vladimir Skomorokhov <vizion84@yandex.ru>
2023-11-19 22:11:11 +0300

    Переделал компонент на использование файла class.php (файл component.php не используется)

--- local/components/wizart/orders_grid/class.php
+++ local/components/wizart/orders_grid/class.php
@@ -5,7 +5,8 @@ if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 use Bitrix\Main\Engine\Contract\Controllerable;
 use Bitrix\Main\ErrorCollection;
 use Bitrix\Main\Engine\ActionFilter;
-
+use Bitrix\Main\Grid\Options as GridOptions;
+use Bitrix\Main\UI\PageNavigation;
 
 \Bitrix\Main\Loader::includeModule('crm');
 
@@ -15,23 +16,23 @@ class AjaxComponent extends CBitrixComponent implements Controllerable
     protected $errorCollection;
     
 
-    public static  $gridName = 'order_grid';
+    private const  GRID_ID = 'order_grid';
 
-    public static $filterName = 'order_filter';
+    private const FILTER_ID = 'order_filter';
 
-    public static  $httpLink = 'https://middle-crm-predprod.middle-task.boxberry.ru/';
+    private static  $httpLink = 'https://middle-crm-predprod.middle-task.boxberry.ru/';
 
-    public static $rest = 'v1/parcels';
+    private static $rest = 'v1/parcels';
 
-    public static  $token = 'Hello';
+    private static  $token = 'Hello';
 
-    public static $arPaymentType = [
+    private static $arPaymentType = [
         1 => 'Предоплата',
         2 => 'Наличные',
         3 => 'Эквайринг',
     ];
 
-    public static $arDeliveryType = [
+    private static $arDeliveryType = [
         0 => 'Не определён',
         1 => 'Доставка до ПВЗ',
         2 => 'Курьерская доставка',
@@ -39,18 +40,27 @@ class AjaxComponent extends CBitrixComponent implements Controllerable
         4 => 'Курьерская доставка 2.0',
     ];
 
-    public static $arIssueType = [
+    private static $arIssueType = [
         0 => "Без вскрытия",
         1 => "Со вскрытием",
         2 => "Частичная выдача",
     ];
 
-    public static  $fild1CName = '';
+    private static  $fild1CName = '';
 
-    public function sortNavGet()
+    public function executeComponent()
     {
+        $this->arResult['list_id'] = static::GRID_ID;
+        $this->arResult['filter_id'] = static::FILTER_ID;
+        $this->arResult['companyID'] = isset($_REQUEST['idCompany']) ? $_REQUEST['idCompany'] : '';
+        $this->arResult['params'] = $this->getParams();
+        $this->arResult['data'] = $this->getData();
+        $this->prepareFilter();
+        $this->prepareGrid();
 
+        $this->includeComponentTemplate();
     }
+
 
     public function onPrepareComponentParams($arParams)
     {
@@ -78,21 +88,26 @@ class AjaxComponent extends CBitrixComponent implements Controllerable
         return $this->errorCollection->toArray();
     }
 
+    private function getParams($inParams = [])
+    {
+        $params['searchParameters.shopCodes'] = 5555555;
+        $params['page.offset'] = 0;
+        $params['page.limit'] = 20;
+        return $params;
+    }
 
     /**
      * Get data from API
      * @params array - array params for link
      * @return data 
      */
-    public function getData(array $params=[]) 
+    private function getData(array $params=[]) 
     {
         $fullLink = self::$httpLink . self::$rest . '?token=' . self::$token;
-        $params['searchParameters.shopCodes'] = 5555555;
-        $params['page.offset'] = 0;
-        $params['page.limit'] = 20;
+        
         $ch = curl_init();
         $token = self::$token;
-        curl_setopt($ch, CURLOPT_URL, $fullLink . '&' . http_build_query($params));
+        curl_setopt($ch, CURLOPT_URL, $fullLink . '&' . http_build_query($this->arResult['params']));
         curl_setopt($ch, CURLOPT_HTTPHEADER, array("Authorization: Bearer {$token}"));
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
@@ -103,13 +118,121 @@ class AjaxComponent extends CBitrixComponent implements Controllerable
         return $data;
     }
 
-    public function getDateTimeStrFromStr(string $stringDate)
+    private function prepareFilter()
+    {
+        $this->arResult['ui_filter'] = [
+            ['id' => 'trackNumber', 'name' => 'Трек-номер', 'type' => 'text', 'default' => true],
+            ['id' => 'invoiceNumber', 'name' => 'Номер ЭН', 'type' => 'text', ],
+            ['id' => 'orderNumber', 'name' => 'Номер заказа ИМ', 'type' => 'text', ],
+            ['id' => 'createDate', 'name' => 'Дата создания заказа', 'type' => 'date', 
+                "exclude" => array(
+                    \Bitrix\Main\UI\Filter\DateType::YESTERDAY,
+                    \Bitrix\Main\UI\Filter\DateType::CURRENT_DAY,
+                    \Bitrix\Main\UI\Filter\DateType::TOMORROW,
+                    \Bitrix\Main\UI\Filter\DateType::CURRENT_WEEK,
+                    \Bitrix\Main\UI\Filter\DateType::CURRENT_MONTH,
+                    \Bitrix\Main\UI\Filter\DateType::CURRENT_QUARTER,
+                    \Bitrix\Main\UI\Filter\DateType::LAST_7_DAYS,
+                    \Bitrix\Main\UI\Filter\DateType::LAST_30_DAYS,
+                    \Bitrix\Main\UI\Filter\DateType::LAST_60_DAYS,
+                    \Bitrix\Main\UI\Filter\DateType::LAST_90_DAYS,
+                    \Bitrix\Main\UI\Filter\DateType::PREV_DAYS,
+                    \Bitrix\Main\UI\Filter\DateType::NEXT_DAYS,
+                    \Bitrix\Main\UI\Filter\DateType::QUARTER,
+                    \Bitrix\Main\UI\Filter\DateType::YEAR,
+                    \Bitrix\Main\UI\Filter\DateType::EXACT,
+                    \Bitrix\Main\UI\Filter\DateType::LAST_WEEK,
+                    \Bitrix\Main\UI\Filter\DateType::LAST_MONTH,
+                    \Bitrix\Main\UI\Filter\DateType::NEXT_WEEK,
+                    \Bitrix\Main\UI\Filter\DateType::NEXT_MONTH,
+                    \Bitrix\Main\UI\Filter\DateType::MONTH,
+                )
+            ],
+            
+        ];
+        
+        $this->arResult['filterOption'] = new Bitrix\Main\UI\Filter\Options($this->arResult['filter_id']);
+        
+        
+        $this->arResult['filterData'] = $this->arResult['filterOption'] -> getFilter([]);
+        
+        foreach ($this->arResult['filterData'] as $k => $v) {
+            if($k == 'FIND' && $v) {
+                $this->arResult['filterData'][] = array(
+                "LOGIC" => "OR",
+                array("trackNumber" => "%".$v."%"),
+                array("invoiceNumber" => "%".$v."%"),
+                array("orderNumber" => "%".$v."%"),
+                array('createDate' => "%".$v."%"),
+                );
+            } else if ($k == 'trackNumber' && $v) {
+                $this->arResult['filterData']['trackNumber'] = "%".$v."%";
+            } else if ($k == 'invoiceNumber' && $v) {
+                $this->arResult['filterData']['invoiceNumber'] = "%".$v."%";
+            } else if ($k == 'orderNumber' && $v) {
+                $this->arResult['filterData']['orderNumber'] = "%".$v."%";
+            } else if ($k == 'createDate_to' && $v) {
+                $this->arResult['filterData']['createDate'] = "%". explode(' ', $v)[0] ."%";
+            }  else {
+                $this->arResult['filterData'][$k] = $v;
+            }
+        }
+        $this->arResult['filterData']['ACTIVE'] = "Y";
+    }
+
+    private function prepareGrid()
+    {
+        $this->arResult['grid_options'] = new GridOptions($this->arResult['list_id']);
+        $this->arResult['sort'] = $this->arResult['grid_options'] -> GetSorting(['sort' => ['DATE_CREATE' => 'DESC'], 'vars' => ['by' => 'by', 'order' => 'order']]);
+
+        $this->arResult['columns'] = [];
+        $this->arResult['columns'][] = ['id' => 'trackNumber', 'name' => 'Трек-номер', 'sort' => 'trackNumber', 'default' => true];
+        $this->arResult['columns'][] = ['id' => 'invoiceNumber', 'name' => 'Номер ЭН', 'sort' => 'invoiceNumber', 'default' => true];
+        $this->arResult['columns'][] = ['id' => 'orderNumber', 'name' => 'Номер заказа ИМ', 'sort' => 'orderNumber', 'default' => true];
+        $this->arResult['columns'][] = ['id' => 'createDate', 'name' => 'Дата создания заказа', 'sort' => 'createDate', 'default' => true];
+        $this->arResult['columns'][] = ['id' => 'storeDate', 'name' => 'Срок хранения', 'sort' => 'storeDate', 'default' => true];
+        $this->arResult['columns'][] = ['id' => 'return', 'name' => 'Признак возврата', 'sort' => 'return', 'default' => true];
+        $this->arResult['columns'][] = ['id' => 'declaredValue', 'name' => 'Объявленная стоимость, руб.', 'sort' => 'declaredValue', 'default' => true];
+        $this->arResult['columns'][] = ['id' => 'amountPay', 'name' => 'Сумма к уплате, руб.', 'sort' => 'amountPay', 'default' => true];
+        $this->arResult['columns'][] = ['id' => 'deliveryCost', 'name' => 'Стоимость доставки, руб.', 'sort' => 'deliveryCost', 'default' => true];
+        $this->arResult['columns'][] = ['id' => 'paymentType', 'name' => 'Тип оплаты', 'sort' => 'paymentType', 'default' => true];
+        $this->arResult['columns'][] = ['id' => 'actualWeight', 'name' => 'Фактический вес, кг', 'sort' => 'actualWeight', 'default' => true];
+        $this->arResult['columns'][] = ['id' => 'deliveryType', 'name' => 'Вид доставки', 'sort' => 'deliveryType', 'default' => true];
+        $this->arResult['columns'][] = ['id' => 'issueType', 'name' => 'Тип выдачи', 'sort' => 'issueType', 'default' => true];
+        $this->arResult['columns'][] = ['id' => 'barcodes', 'name' => 'Места', 'sort' => 'barcodes', 'default' => true];
+
+        $data = $this->arResult['data']['parcels'];
+        for ($i = 0; $i < count($data); $i++) {
+        
+            $this->arResult['list'][] = [
+                'data' => [
+                            "trackNumber" => $data[$i]['trackNumber'],
+                            "invoiceNumber" => $data[$i]['invoiceNumber'],
+                            "orderNumber" => $data[$i]['orderNumber'],
+                            "createDate" => $this->getDateTimeStrFromStr($data[$i]['createDate']),
+                            "storeDate" => $data[$i]['storeDate'],
+                            "return" => $data[$i]['return'] ? 'Да' : 'Нет',
+                            "declaredValue" => $this->getStrCost($data[$i]['declaredValue']),
+                            "amountPay" => $this->getStrCost($data[$i]['amountPay']),
+                            "deliveryCost" => $this->getStrCost($data[$i]['deliveryCost']),
+                            "paymentType" => $this->getPaymentType($data[$i]['paymentType']),
+                            "actualWeight" => $data[$i]['actualWeight'],
+                            "deliveryType" => $this->getDeliveryType($data[$i]['deliveryType']),
+                            "issueType" => $this->getIssueType($data[$i]['issueType']),
+                            "barcodes" => count($data[$i]['barcodes']),
+                    ],
+            ];
+            
+        }
+    }
+    
+    private function getDateTimeStrFromStr(string $stringDate)
     {
         $dateObj = date_create($stringDate);
         return date_format($dateObj, "d.m.Y H:i:s");
     }
 
-    public function getStrCost(int $intCost)
+    private function getStrCost(int $intCost)
     {
         if ($intCost === 0) {
             return 0;
@@ -143,7 +266,7 @@ class AjaxComponent extends CBitrixComponent implements Controllerable
         return $strCostResult;
     }
 
-    public function getPaymentType(int $numberPaymentType)
+    private function getPaymentType(int $numberPaymentType)
     {
         if (array_key_exists($numberPaymentType, self::$arPaymentType)) {
             return self::$arPaymentType[$numberPaymentType];
@@ -151,7 +274,7 @@ class AjaxComponent extends CBitrixComponent implements Controllerable
         return $numberPaymentType;
     }
 
-    public function getDeliveryType(int $numberDeliveryType)
+    private function getDeliveryType(int $numberDeliveryType)
     {
         if (array_key_exists($numberDeliveryType, self::$arDeliveryType)) {
             return self::$arDeliveryType[$numberDeliveryType];
@@ -159,7 +282,7 @@ class AjaxComponent extends CBitrixComponent implements Controllerable
         return $numberDeliveryType;
     }
 
-    public function getIssueType(array $arData)
+    private function getIssueType(array $arData)
     {
         if (array_key_exists($arData[0], self::$arIssueType)) {
             return self::$arIssueType[$arData[0]];
--- local/components/wizart/orders_grid/component.php
+++ local/components/wizart/orders_grid/component.php
@@ -11,115 +11,17 @@ $APPLICATION->SetTitle("Orders");
 use Bitrix\Main\Grid\Options as GridOptions;
 use Bitrix\Main\UI\PageNavigation;
 
-$arResult['companyID'] = isset($_REQUEST['idCompany']) ? $_REQUEST['idCompany'] : '';
+/*$arResult['companyID'] = isset($_REQUEST['idCompany']) ? $_REQUEST['idCompany'] : '';
 
 $arResult['data'] = $this->getData();
 
 $arResult['list_id'] = $this::$gridName;
-$arResult['filter_id'] = $this::$filterName;
-
-$arResult['grid_options'] = new GridOptions($arResult['list_id']);
-$arResult['sort'] = $arResult['grid_options'] -> GetSorting(['sort' => ['DATE_CREATE' => 'DESC'], 'vars' => ['by' => 'by', 'order' => 'order']]);
-
-$arResult['columns'] = [];
-$arResult['columns'][] = ['id' => 'trackNumber', 'name' => 'Трек-номер', 'sort' => 'trackNumber', 'default' => true];
-$arResult['columns'][] = ['id' => 'invoiceNumber', 'name' => 'Номер ЭН', 'sort' => 'invoiceNumber', 'default' => true];
-$arResult['columns'][] = ['id' => 'orderNumber', 'name' => 'Номер заказа ИМ', 'sort' => 'orderNumber', 'default' => true];
-$arResult['columns'][] = ['id' => 'createDate', 'name' => 'Дата создания заказа', 'sort' => 'createDate', 'default' => true];
-$arResult['columns'][] = ['id' => 'storeDate', 'name' => 'Срок хранения', 'sort' => 'storeDate', 'default' => true];
-$arResult['columns'][] = ['id' => 'return', 'name' => 'Признак возврата', 'sort' => 'return', 'default' => true];
-$arResult['columns'][] = ['id' => 'declaredValue', 'name' => 'Объявленная стоимость, руб.', 'sort' => 'declaredValue', 'default' => true];
-$arResult['columns'][] = ['id' => 'amountPay', 'name' => 'Сумма к уплате, руб.', 'sort' => 'amountPay', 'default' => true];
-$arResult['columns'][] = ['id' => 'deliveryCost', 'name' => 'Стоимость доставки, руб.', 'sort' => 'deliveryCost', 'default' => true];
-$arResult['columns'][] = ['id' => 'paymentType', 'name' => 'Тип оплаты', 'sort' => 'paymentType', 'default' => true];
-$arResult['columns'][] = ['id' => 'actualWeight', 'name' => 'Фактический вес, кг', 'sort' => 'actualWeight', 'default' => true];
-$arResult['columns'][] = ['id' => 'deliveryType', 'name' => 'Вид доставки', 'sort' => 'deliveryType', 'default' => true];
-$arResult['columns'][] = ['id' => 'issueType', 'name' => 'Тип выдачи', 'sort' => 'issueType', 'default' => true];
-$arResult['columns'][] = ['id' => 'barcodes', 'name' => 'Места', 'sort' => 'barcodes', 'default' => true];
-
-$data = $arResult['data']['parcels'];
-for ($i = 0; $i < count($data); $i++) {
-  
-	$arResult['list'][] = [
-		'data' => [
-					"trackNumber" => $data[$i]['trackNumber'],
-					"invoiceNumber" => $data[$i]['invoiceNumber'],
-					"orderNumber" => $data[$i]['orderNumber'],
-					"createDate" => $this->getDateTimeStrFromStr($data[$i]['createDate']),
-					"storeDate" => $data[$i]['storeDate'],
-					"return" => $data[$i]['return'] ? 'Да' : 'Нет',
-					"declaredValue" => $this->getStrCost($data[$i]['declaredValue']),
-					"amountPay" => $this->getStrCost($data[$i]['amountPay']),
-					"deliveryCost" => $this->getStrCost($data[$i]['deliveryCost']),
-					"paymentType" => $this->getPaymentType($data[$i]['paymentType']),
-					"actualWeight" => $data[$i]['actualWeight'],
-					"deliveryType" => $this->getDeliveryType($data[$i]['deliveryType']),
-					"issueType" => $this->getIssueType($data[$i]['issueType']),
-					"barcodes" => count($data[$i]['barcodes']),
-			],
-	];
-	
-}
+$arResult['filter_id'] = $this::$filterName;*/
 
 
-$arResult['ui_filter'] = [
-	['id' => 'trackNumber', 'name' => 'Трек-номер', 'type' => 'text', 'default' => true],
-	['id' => 'invoiceNumber', 'name' => 'Номер ЭН', 'type' => 'text', ],
-	['id' => 'orderNumber', 'name' => 'Номер заказа ИМ', 'type' => 'text', ],
-	['id' => 'createDate', 'name' => 'Дата создания заказа', 'type' => 'date', 
-		"exclude" => array(
-			\Bitrix\Main\UI\Filter\DateType::YESTERDAY,
-			\Bitrix\Main\UI\Filter\DateType::CURRENT_DAY,
-			\Bitrix\Main\UI\Filter\DateType::TOMORROW,
-			\Bitrix\Main\UI\Filter\DateType::CURRENT_WEEK,
-			\Bitrix\Main\UI\Filter\DateType::CURRENT_MONTH,
-			\Bitrix\Main\UI\Filter\DateType::CURRENT_QUARTER,
-			\Bitrix\Main\UI\Filter\DateType::LAST_7_DAYS,
-			\Bitrix\Main\UI\Filter\DateType::LAST_30_DAYS,
-			\Bitrix\Main\UI\Filter\DateType::LAST_60_DAYS,
-			\Bitrix\Main\UI\Filter\DateType::LAST_90_DAYS,
-			\Bitrix\Main\UI\Filter\DateType::PREV_DAYS,
-			\Bitrix\Main\UI\Filter\DateType::NEXT_DAYS,
-			\Bitrix\Main\UI\Filter\DateType::QUARTER,
-			\Bitrix\Main\UI\Filter\DateType::YEAR,
-			\Bitrix\Main\UI\Filter\DateType::EXACT,
-			\Bitrix\Main\UI\Filter\DateType::LAST_WEEK,
-			\Bitrix\Main\UI\Filter\DateType::LAST_MONTH,
-			\Bitrix\Main\UI\Filter\DateType::NEXT_WEEK,
-			\Bitrix\Main\UI\Filter\DateType::NEXT_MONTH,
-			\Bitrix\Main\UI\Filter\DateType::MONTH,
-		)
-	],
-	
-];
-
-$arResult['filterOption'] = new Bitrix\Main\UI\Filter\Options($arResult['filter_id']);
 
 
-$arResult['filterData'] = $arResult['filterOption'] -> getFilter([]);
 
-foreach ($arResult['filterData'] as $k => $v) {
-	if($k == 'FIND' && $v) {
-		$arResult['filterData'][] = array(
-		"LOGIC" => "OR",
-		array("trackNumber" => "%".$v."%"),
-		array("invoiceNumber" => "%".$v."%"),
-		array("orderNumber" => "%".$v."%"),
-		array('createDate' => "%".$v."%"),
-		);
-	} else if ($k == 'trackNumber' && $v) {
-		$arResult['filterData']['trackNumber'] = "%".$v."%";
-	} else if ($k == 'invoiceNumber' && $v) {
-		$arResult['filterData']['invoiceNumber'] = "%".$v."%";
-	} else if ($k == 'orderNumber' && $v) {
-		$arResult['filterData']['orderNumber'] = "%".$v."%";
-	} else if ($k == 'createDate_to' && $v) {
-		$arResult['filterData']['createDate'] = "%". explode(' ', $v)[0] ."%";
-	}  else {
-		$arResult['filterData'][$k] = $v;
-	}
-}
-$arResult['filterData']['ACTIVE'] = "Y";
 
 /*
 
@@ -208,4 +110,4 @@ $arResult['filterData']['ACTIVE'] = "Y";
 
 */
 
-$this->includeComponentTemplate();
+//$this->includeComponentTemplate();
