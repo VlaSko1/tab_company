9c89b568324ec6060bb843d3fec71a2a1f9fced0
Vladimir Skomorokhov <vizion84@yandex.ru>
2023-12-04 15:37:50 +0300

    изменил обработчик события (теперь в функцию-обработчик прилетает именно событие), изменил саму функцию-обработчик: добавил класс ReflectionClass

--- local/php_interface/events.php
+++ local/php_interface/events.php
@@ -1,3 +1,7 @@
 <? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 
-AddEventHandler('crm', 'onEntityDetailsTabsInitialized', 'onEntityDetailsTabs');
+//AddEventHandler('crm', 'onEntityDetailsTabsInitialized', 'onEntityDetailsTabs');
+
+// Отлавливаем событие построения табов 
+$eventManager = \Bitrix\Main\EventManager::getInstance();
+$eventManager -> AddEventHandler('crm', 'onEntityDetailsTabsInitialized', 'onEntityDetailsTabs');
--- local/php_interface/functions.php
+++ local/php_interface/functions.php
@@ -1,7 +1,7 @@
 <? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true)
     die();
 
-function onEntityDetailsTabs($entityID, $entityTypeID, $eventName, $eventTab) 
+/*function onEntityDetailsTabs($entityID, $entityTypeID, $eventName, $eventTab) 
 {
     if ($entityTypeID === 4) {
         
@@ -27,4 +27,39 @@ function onEntityDetailsTabs($entityID, $entityTypeID, $eventName, $eventTab)
             'tabs' => $tabs,
         ]);
     }
-}
+}*/
+
+function onEntityDetailsTabs($event)
+{
+    $entityId = $event->getParameter('entityID');
+    $entityTypeID = $event->getParameter('entityTypeID');
+    $tabs = $event->getParameter('tabs');
+
+    $siteID = isset($_REQUEST['site']) ? mb_substr(preg_replace('/[^a-z0-9_]/i', '', $_REQUEST['site']), 0, 2) : '';
+
+    if ($siteID !== '') {
+        define('SITE_ID', $siteID);
+    }
+
+    $reflection = new \ReflectionClass($event);
+    $property = $reflection->getProperty('parameters');
+    $property->setAccessible(true);
+
+    $eventParameters = $property->getValue($event);
+
+    $idCompany = $entityID;
+    $tabs[] = array (                            
+        'id' => 'store_orders',
+         'name' => 'Заказы интернет-магазина',
+         'loader' => array(
+             'serviceUrl' => '/local/components/wizart/orders_grid/lazyload.ajax.php?site=' . 
+                 \SITE_ID . '&' . \bitrix_sessid_get() . '&idCompany=' . $idCompany ),
+    );
+
+    $eventParameters['tabs'] = $tabs;
+    $property->setValue($event, $eventParameters);
+
+    return new \Bitrix\Main\EventResult(\Bitrix\Main\EventResult::SUCCESS, [
+        'tabs' => $tabs,
+    ]);
+} 
